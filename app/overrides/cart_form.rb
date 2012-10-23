1c1df22af3a66c0978d65e692223a15b33fffcfc
Robert Oles <robertoles@me.com>
2012-10-23 15:41:12 +0100

    Fixing adding base product to cart

--- app/overrides/cart_form.rb
+++ app/overrides/cart_form.rb
@@ -2,3 +2,8 @@ Deface::Override.new(:virtual_path => "spree/products/_cart_form",
                      :name => "add_related_variants_cart",
                      :insert_after => "code[erb-silent]:contains('if @product.has_variants?')",
                      :partial => "spree/products/related_variants_cart")
+
+Deface::Override.new(:virtual_path => "spree/products/_cart_form",
+                     :name => "ensure_product_added_to_cart",
+                     :insert_top => ".add-to-cart",
+                     :text => '<%= hidden_field_tag "add_variants[#{@product.master.id}]" %>')
