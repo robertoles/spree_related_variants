7b01f448c9bd957d584a6f3181c337e0854b8554
Robert Oles <robertoles@me.com>
2012-10-24 16:27:23 +0100

    Bug fix

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -5,6 +5,7 @@ Spree::OrdersController.class_eval do
   private
 
   def remove_unwanted_variants
+    return unless params[:variants]
     params[:add_variants] = {} unless params[:add_variants]
     params[:variants].delete_if do |key, value|
       params[:add_variants][key].nil?
